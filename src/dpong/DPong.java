661b625824a10d3277af90a22e1502d4d510546f
András Márki <b10andma@student.his.se>
2012-04-04 20:15:41 +0200

    One player
    Keylistener to player.

--- src/dpong/DPong.java
+++ src/dpong/DPong.java
@@ -3,12 +3,14 @@ package dpong;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.util.LinkedList;
 import javax.swing.*;
 
-public class DPong extends JFrame implements WindowListener {
+public class DPong extends JFrame implements WindowListener, KeyListener {
 	
 	private LinkedList<Entity> m_entities;
 	private boolean windowClosed = false;
@@ -16,8 +18,13 @@ public class DPong extends JFrame implements WindowListener {
 	private Image m_image;
 	private Graphics m_graphics;
 	
+	private Player m_player1;
+	
 	public DPong() {
 		this.addWindowListener(this);
+		this.addKeyListener(this);
+		
+		
 		this.setIgnoreRepaint(true);
 		this.setBackground(Color.BLACK);
 		m_entities = new LinkedList<Entity>();
@@ -25,7 +32,13 @@ public class DPong extends JFrame implements WindowListener {
 		this.setResizable(false);
 		this.setTitle("DPong");
 		this.setVisible(true);
+		
 		m_entities.add(new Ball());
+		m_player1 = new Player(30, false);
+		m_entities.add(m_player1);
+		
+		
+		
 		m_image = this.createImage(800, 600);
 		m_graphics = m_image.getGraphics();
 	}
@@ -90,6 +103,30 @@ public class DPong extends JFrame implements WindowListener {
 
 	@Override
 	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		if (arg0.getKeyCode() == KeyEvent.VK_I) {
+			m_player1.moveY(-10);
+		}
+		else if (arg0.getKeyCode() == KeyEvent.VK_K){
+			m_player1.moveY(10);
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
--- src/dpong/Player.java
+++ src/dpong/Player.java
@@ -1,18 +1,27 @@
 package dpong;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 public class Player extends Entity {
 
 	private final int m_distanceFromBorder = 0;
-	private Vector m_vector;
+	private final int m_width = 10;
+	private final int m_height = 30;
 	
-	public Player(){
-				
+	public Player(int xPos, boolean isOnTheLeftSide){
+		int multiplier = 1;
+		if(!isOnTheLeftSide){
+			multiplier = -1;			
+		}
+		
+		m_vector = new Vector(xPos +(multiplier * m_distanceFromBorder), 300);
 	}
 	
 	public void moveY(int y) {
-		
+		int newY = m_vector.getY() + y;
+		if (newY > 0 && newY < 600-m_height)
+			m_vector.setY(newY);
 	}
 	
 	@Override
@@ -30,7 +39,8 @@ public class Player extends Entity {
 	@Override
 	public void draw(float interpolation, Graphics g) {
 		// TODO Auto-generated method stub
-		
+		g.setColor(Color.PINK);
+		g.fillRect(m_vector.getX(), m_vector.getY(), m_width, m_height);		
 	}
 
 	@Override
