e2c606d9e8439fb7b70273c547b8b93c293ea74a
András Márki <b10andma@student.his.se>
2012-04-11 19:17:54 +0200

    second part...

---
+++ src/dpong/Setup.java
@@ -0,0 +1,14 @@
+package dpong;
+
+public class Setup {
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 600;
+	
+	public int getWidth(){
+		return WIDTH;		
+	}
+	
+	public int getHeight(){
+		return HEIGHT;		
+	}
+}
